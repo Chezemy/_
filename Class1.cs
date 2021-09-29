5c9d2f03c77198964ebc63989f70a3e862ae2d19
Панюшкин Павел <Parframe@gmail.com>
2021-09-29 09:16:51 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Class1
+    {
+
+    }
+
+    public class CalculatorChecker
+    {
+        public static bool validateOperation(double num1, double num2, string operation, double result)
+        {
+            if (operation == "+")
+            {
+                return (num1 + num2 == result);
+            }
+
+            if (operation == "-")
+            {
+                return (num1 - num2 == result);
+            }
+
+            if (operation == "*")
+            {
+                return (num1 * num2 == result);
+            }
+
+            if (operation == "/")
+            {
+                return (num1 / num2 == result);
+            }
+
+
+
+            return true;
+        }
+    }
+}
